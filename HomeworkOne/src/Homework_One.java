9b9f945ddf9c63d15bb913a46f65bf8eec267a69
Chillers1101 <chillers1101cj@gmail.com>
2017-10-20 17:27:34 -0700

    Input Validation

--- HomeworkOne/src/Homework_One.java
+++ HomeworkOne/src/Homework_One.java
@@ -1,19 +1,24 @@
-
 import java.util.Scanner;
-
-
-
 
 public class Homework_One {
 	
 	public static boolean isPlural(int value) {
-		
-		if(value > 1) {
-			return true;
-		}
-		
-		return false;
+	    return (value > 1);	
 	}
+
+    public static boolean isEmpty(int value) {
+        return (value == 0);
+    }
+
+    public static void printStatement(String measurement, int value) {
+        if (isEmpty(value)) {
+            System.out.print("");
+        } else if (isPlural(value)) {
+            System.out.print(value + " " + measurement + "s ");
+        } else {
+            System.out.print(value + " " + measurement + " ");
+        }
+    }
 	
 	public static void main(String[] args) {
 		
@@ -21,53 +26,41 @@ public class Homework_One {
 		int hours, minutes, seconds;
 		Scanner scan = new Scanner(System.in);
 	      
-	    System.out.println("Please enter the lot's length and width: ");
+	    System.out.print("Lot's length and width: ");
 	    length1 = scan.nextDouble();
 	    width1 = scan.nextDouble();
 	    
-	    System.out.println("Now enter the house's length: ");
+	    System.out.print("House's length and width: ");
 	    length2 = scan.nextDouble();
 	    width2 = scan.nextDouble();
 	    
-	    System.out.println("Now lastly, enter the rate at which the lawn is being mown: ");
-	    rate = scan.nextDouble();
-	    
 	    lawnarea = (double)(length1 * width1) - (length2 * width2);
-	    System.out.println("The area of the lawn is " +lawnarea+ " feet squared.");
 	    
-	    seconds = (int)Math.round(lawnarea/rate);
+	    if(lawnarea == 0) {
+	    		System.out.println("There is no lawn.");
+	    } else if(lawnarea < 0) {
+	    		System.out.println("Those are not valid dimensions.");
+	    } else {
+	    		System.out.println("Area of lawn: " +lawnarea+ " ft^2");
+	    		
+	    		System.out.print("Rate at which the lawn is being mown: ");
+	    	    rate = scan.nextDouble();
+	    		
+	    		seconds = (int)Math.round(lawnarea/rate);
+	    	    
+	    	    minutes = seconds/60;
+	    	    seconds = seconds%60;
+	    	    hours = minutes/60;
+	    	    minutes = minutes%60;
+	    	  
+	        System.out.print("Lawn mowed in ");
+	        printStatement("hour", hours);
+	        printStatement("minute", minutes);
+	        printStatement("second", seconds);
+	    	    System.out.println("");
+	    }
 	    
-	    minutes = seconds/60;
-	    seconds = seconds%60;
-	    hours = minutes/60;
-	    minutes = minutes%60;
 	    
-	    if( seconds==1 && hours==1 && minutes==1) { 
-	        System.out.print("The amount of time to mow the lawn will be ");
-	        System.out.println(hours+" hour "+minutes+" minute "+seconds+" second. ");
-	        } else if (seconds ==1 && hours==1) { 
-	        System.out.print("The amount of time to mow the lawn will be ");
-	        System.out.println(hours+" hour "+minutes+" minutes "+seconds+" second. ");
-	        } else if (seconds==1 && minutes==1) {
-	        System.out.print("The amount of time to mow the lawn will be ");
-	        System.out.println(hours+" hours "+minutes+" minute "+seconds+" second. ");
-	        } else if (hours==1 && minutes==1 ) {
-	        System.out.print("The amount of time to mow the lawn will be ");
-	        System.out.println(hours+" hour "+minutes+" minute "+seconds+" seconds. ");
-	        } else if (seconds==1) {
-	        System.out.print("The amount of time to mow the lawn will be ");
-	        System.out.println(hours+" hours "+minutes+" minutes "+seconds+" second. ");
-	        } else if (hours==1) {
-	        System.out.print("The amount of time to mow the lawn will be ");
-	        System.out.println(hours+" hour "+minutes+" minutes "+seconds+" seconds. ");
-	        } else if (minutes==1) {
-	        System.out.print("The amount of time to mow the lawn will be ");
-	        System.out.println(hours+" hours "+minutes+" minute "+seconds+" seconds. ");
-	        } else {
-	        System.out.print("The amount of time to mow the lawn will be ");
-	        System.out.println(hours+" hours "+minutes+" minutes "+seconds+" seconds. ");
-	        
-	        }
-
-	}
+	   
+    }
 }
